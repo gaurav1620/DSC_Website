d405cf0aace4f43ba5807e687da78e3fdf07c2fe
Rohit Dalvi <fswdev18@gmail.com>
2020-09-25 15:55:17 +0530

    Updates

---
+++ precache-manifest.2532257aa0b995e12f566950d2f5b5b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a4389c97b401a0fffb1fe5a22833e9",
+    "url": "/DSC_Website/index.html"
+  },
+  {
+    "revision": "03f5020e713881585a5c",
+    "url": "/DSC_Website/static/css/main.77bb3d0f.chunk.css"
+  },
+  {
+    "revision": "eae0a1437189075b86c9",
+    "url": "/DSC_Website/static/js/2.fbc02e30.chunk.js"
+  },
+  {
+    "revision": "9ee1d11e4fb7fb7378917db6bce4eed8",
+    "url": "/DSC_Website/static/js/2.fbc02e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f5020e713881585a5c",
+    "url": "/DSC_Website/static/js/main.6e6cd14b.chunk.js"
+  },
+  {
+    "revision": "de53939ebe9346f3d78c",
+    "url": "/DSC_Website/static/js/runtime-main.d9653d25.js"
+  },
+  {
+    "revision": "95c010f1e523daf259fe1d2607645f47",
+    "url": "/DSC_Website/static/media/DSCPVGCOETHorizontal.95c010f1.png"
+  },
+  {
+    "revision": "973fc04b3aa04afe7a4911bfe0eeb78a",
+    "url": "/DSC_Website/static/media/DSCPVGCOETVertical.973fc04b.png"
+  },
+  {
+    "revision": "371a5ea2b9b9c08f64abf13ee4965623",
+    "url": "/DSC_Website/static/media/gaurav_khairnar.371a5ea2.jpg"
+  },
+  {
+    "revision": "799ad44c7d92b760cf75c2106c8f8e5a",
+    "url": "/DSC_Website/static/media/homepage_main.799ad44c.jpg"
+  },
+  {
+    "revision": "7e902205a02291249f6e1f6fc90d3a22",
+    "url": "/DSC_Website/static/media/jigsaw.7e902205.png"
+  },
+  {
+    "revision": "9804ab6c5bc1c22b1d22b05d4638fee1",
+    "url": "/DSC_Website/static/media/lightbulb.9804ab6c.png"
+  },
+  {
+    "revision": "2b7a22146b2b566d5d5bf3c5caadb15d",
+    "url": "/DSC_Website/static/media/sunanda_somwase.2b7a2214.jpeg"
+  },
+  {
+    "revision": "4dd2f5525adf1d4a806d4f6dbacfcc29",
+    "url": "/DSC_Website/static/media/team.4dd2f552.jpg"
+  },
+  {
+    "revision": "b6d114870cdfe5e8e7ad39c5429f6607",
+    "url": "/DSC_Website/static/media/team.b6d11487.png"
+  }
+]);
